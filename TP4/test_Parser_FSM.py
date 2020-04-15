a9eb765ffba5cc21308c8064385276b5f1c97135
Thomas Houtart <thomas.houtart@hotmail.com>
2020-04-15 13:43:04 -0400

    TH [DEV]: added other tests to the mix

--- TP4/test_Parser_FSM.py
+++ TP4/test_Parser_FSM.py
@@ -1,7 +1,7 @@
 import unittest
 import unittest.mock
 import os
-from Parser_FSM import ApplyRules, RuleGroup, Rule, S_NEW_GROUP, S_PRE
+from Parser_FSM import ApplyRules, RuleGroup, Rule, S_NEW_GROUP, S_PRE, S_SUBJ, S_END_RULE, S_END_GROUP, S_OP
 from unittest.mock import MagicMock, Mock
 
 class TestParserFSM(unittest.TestCase):
@@ -15,16 +15,65 @@ class TestParserFSM(unittest.TestCase):
         print(parse.current_state)
         self.assertEqual(parse.current_state, S_NEW_GROUP)
 
-    def test_when_value_is_a_letter_state_should_be_pre(self):
+    def test_when_first_char_is_a_letter_state_should_move_to_pre(self):
         test_string = "p"
         parse = ApplyRules(test_string)
         parse.run()
         print(parse.current_state)
         self.assertEqual(parse.current_state, S_PRE)
+
+    def test_when_char_does_not_start_with_a_letter_state_should_be_new_group(self):
+        test_string = "("
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_NEW_GROUP)
     
-    def test_when_value_starts_with_a_letter_and_is_followed_by_letters_state_should_stay_in_pre(self):
-        test_string = "print"
+    def test_when_value_started_with_a_letter_and_is_followed_by_letters_state_should_stay_in_pre(self):
+        test_string = "pr"
         parse = ApplyRules(test_string)
         parse.run()
         print(parse.current_state)
         self.assertEqual(parse.current_state, S_PRE)
+
+    def test_when_adding_a_left_bracket_to_a_value_a_pre_state_shoud_change_to_subject_state(self):
+        test_string = "print("
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_SUBJ)
+
+    def test_when_adding_characters_beside_right_bracket_to_value_in_subject_state_should_stay_in_subject_state(self):
+        test_string = "print(test"
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_SUBJ)
+
+    def test_when_adding_right_bracket_to_value_in_subject_state_should_change_to_end_rule(self):
+        test_string = "print(test)"
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_END_RULE)
+
+    def test_when_adding_right_bracket_to_value_in_end_rule_state_should_change_to_end_group(self):
+        test_string = "print(test))"
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_END_GROUP)
+
+    def test_when_adding_and_operators_to_value_in_end_group_state_should_change_to_op(self):
+        test_string = "print(test))&"
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_OP)
+
+    def test_when_adding_or_operators_to_value_in_end_group_state_should_change_to_op(self):
+        test_string = "print(test))|"
+        parse = ApplyRules(test_string)
+        parse.run()
+        print(parse.current_state)
+        self.assertEqual(parse.current_state, S_OP)
